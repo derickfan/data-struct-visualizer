9342014b6cc20f8866e266d16dc13102c3f86ba1
derickfan <fanderick9999@gmail.com>
2021-01-12 12:17:55 -0500

    move App.js logic to Terminal.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ const App = () => {
 		height: window.innerHeight,
 	});
 
-	const [structure, setStructure] = useState(Structure["LinkedList"]);
-
 	const [nodes, setNodes] = useState([]);
 	const [links, setLinks] = useState([]);
 
@@ -30,40 +28,10 @@ const App = () => {
 		};
 	}, []);
 
-	const runCommand = (command) => {
-		const args = command.trim().split(" ");
-		const func = args.shift();
-		console.log(func);
-		if (!structure) {
-			if (Structure[func]) {
-				setStructure(Structure[func]);
-			}
-		} else if (func === "help") {
-			return printFunctions();
-		} else if (structure.public[func]) {
-			const res = structure.public[func]["func"](...args);
-			setNodes(structure.private["toArray"]);
-			setLinks(structure.private["getLinks"]);
-			return res;
-		} else {
-			return "Command does not exist";
-		}
-	};
-
-	const printFunctions = () => {
-		const funcs = structure.public;
-		console.log(funcs);
-		const arr = Object.keys(funcs).map((e) => {
-			return funcs[e]["description"];
-		});
-		arr.unshift("List of Parameters");
-		return arr.join("\r\n");
-	};
-
 	return (
 		<div className="app">
 			<Display nodes={nodes} links={links} windowSize={windowSize} />
-			<Terminal runCommand={runCommand} windowSize={windowSize} />
+			<Terminal setLinks={setLinks} setNodes={setNodes} windowSize={windowSize} />
 		</div>
 	);
 };
--- src/components/Terminal/Terminal.js
+++ src/components/Terminal/Terminal.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
 import { XTerm } from "xterm-for-react";
 import { FitAddon } from "xterm-addon-fit";
+import Structure from "../../lib/Structures";
 import "./Terminal.css";
 
 const Terminal = (props) => {
 	const [input, setInput] = useState("");
 	const [fitAddon, setFitAddon] = useState(new FitAddon());
+	const [structure, setStructure] = useState();
+	const [methods, setMethods] = useState();
 
 	const xterm = useRef(null);
 	const userInput = useRef(null);
@@ -24,7 +27,7 @@ const Terminal = (props) => {
 		});
 		terminal.loadAddon(fitAddon);
 
-		terminal.onKey(keyPressListener);
+		// console.log(xterm.current);
 
 		terminal.write("Enter 'help' to get list a of commands\n\r");
 	}, []);
@@ -33,26 +36,73 @@ const Terminal = (props) => {
 		fitAddon.fit();
 	}, [props.windowSize, FitAddon]);
 
-	const keyPressListener = (event) => {
-		const terminal = xterm.current.terminal;
-		const key = event.domEvent.key;
-		if (key === "Enter") {
-			terminal.write("\r\n");
-			const command = userInput.current.value;
-			const res = props.runCommand(command);
-			terminal.write(res + "\r\n");
-			setInput("");
-		} else if (key === "Backspace") {
-			terminal.write("\b \b");
-			setInput((input) => {
-				return input.substring(0, input.length - 1);
-			});
+	useEffect(() => {
+		const keyPressListener = (event) => {
+			const terminal = xterm.current.terminal;
+			const key = event.domEvent.key;
+			if (key === "Enter") {
+				terminal.write("\r\n");
+				const command = userInput.current.value;
+				let res;
+				if (Structure[command]) {
+					setStructure(Structure[command])
+					res = `${command} has been selected`;
+				} else if (command === "exit") {
+					setStructure();
+					props.setNodes([]);
+					props.setLinks([]);
+					res = "Programm has been reset";
+				} else {
+					res = runCommand(command);
+				}
+				terminal.write(res + "\r\n");
+				setInput("");
+			} else if (key === "Backspace") {
+				terminal.write("\b \b");
+				setInput((input) => {
+					return input.substring(0, input.length - 1);
+				});
+			} else {
+				terminal.write(key);
+				setInput((input) => {
+					return input + key;
+				});
+			}
+		};
+
+		const dispose = xterm.current.terminal.onKey(keyPressListener).dispose;
+		return () => {
+			dispose();
+		};
+	}, [structure]);
+
+	const runCommand = (input) => {
+		const args = input.trim().split(" ");
+		const command = args.shift();
+
+		if (command === "help") {
+			return structure ? printFunctions() : printDataStructure();
+		} else if (structure.public[command]) {
+			const res = structure.public[command]["function"](...args);
+			props.setNodes(structure.private["toArray"]());
+			props.setLinks(structure.private["getLinks"]());
+			return res;
 		} else {
-			terminal.write(key);
-			setInput((input) => {
-				return input + key;
-			});
+			return "Command does not exist";
 		}
+	};
+
+	const printFunctions = () => {
+		const functions = structure.public;
+		const arr = Object.keys(functions).map((e) => {
+			return functions[e]["description"];
+		});
+		arr.unshift("List of Parameters:");
+		return arr.join("\r\n");
+	};
+
+	const printDataStructure = () => {
+		return `List of Data Structures:\r\n${Object.keys(Structure).join("\r\n")}`;
 	};
 
 	return (
